ebba5bf65a194509af6b837b2d822eeb1332dd7f
sude tacer <112017756+sudesud@users.noreply.github.com>
2025-07-08 23:15:55 +0300

    homepage baslandi

---
+++ src/assets/assets.js
@@ -0,0 +1,15 @@
+import blog_icon from './blog_icon.png'
+import arrow from './arrow.png'
+import background from './background.jpg'
+import star from './star.png'
+
+
+
+
+
+export const assets={
+    blog_icon,
+    arrow,
+    background,
+    star
+    }
