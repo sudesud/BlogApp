8f0a188193a3a17a043291e8d311169dac4b5151
sude tacer <112017756+sudesud@users.noreply.github.com>
2025-09-06 23:34:47 +0300

    backend ve frontend birlestirilme devam

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { assets } from '../assets/assets'
+import { useAppContext } from '../context/AppContext'
 
 const Header = () => {
+     const {setInput,input}=useAppContext()
+     const inputRef=useRef()
+
+     const onSubmitHandler=async (e)=>{
+        e.preventDefault();
+        setInput(inputRef.current.value)
+     }
+
+     const onClear =()=>{
+      setInput('')
+      inputRef.current.value=''
+     }
   return (
     <div className='mx-8 sm:mx-16 xl:mx-24 relative'>
       <div className=' text-center mt-20 mb-8'>
@@ -14,15 +27,21 @@ const Header = () => {
         <h1 className='text-3xl sm:text-6xl font-semibold sm:leading-16 text-gray-700'>Your own <span className='text-primary'>blogging</span> <br /> platform.</h1>
        <p className='my-6 sm:my-8 max-w-2xl m-auto max-sm:text-xs text-gray-500'>This is your space to think out loud,to share what matters,and to write witout filters.
         Whether it's one word or thosund,your story starts right here.</p>
-        <form className='flex justify-between max-w-lg max-sm:scale-75 mx-auto border
+
+        <form onSubmit={onSubmitHandler} className='flex justify-between max-w-lg max-sm:scale-75 mx-auto border
         border-gray-300 bg-white rounded overflow-hidden' >
-            <input type="text" placeholder='Search for blogs' required className='w-full pl-4 outline-none' />
+            <input ref={inputRef} type="text" placeholder='Search for blogs' required className='w-full pl-4 outline-none' />
             <button type='submit' className='bg-primary text-white px-8 py-2 m-1.5 rounded
             hover:scale-105 transition-all cursor-pointer'>Search</button>
         </form>
 
       </div>
-
+        <div className='text-center'>
+         {
+         input && <button  onClick={onClear} className='border font-light text-xs py-1 px-3 
+         rounded-sm shadow-custom-sm cursor-pointer'>Clear Search</button>
+         }
+        </div>
       <img src={assets.background} alt="" className='absolute -top-50 -z-1 opacity-50' />
     </div>
   )
