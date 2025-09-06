abbb9dac8d1c9a41853afe484b92446f620c4fde
sude tacer <112017756+sudesud@users.noreply.github.com>
2025-09-06 20:22:54 +0300

    comments ve blog kisimlarinin get post kisimlari yapildi

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,11 +1,15 @@
 import express from "express"
-import { addBlog } from "../controllers/blogController.js";
+import { addBlog, addComment, deleteBlogById, getAllBlogs, getBlogById, getBlogComments, togglePublished } from "../controllers/blogController.js";
 import upload from "../middleware/multer.js";
 import auth from "../middleware/auth.js";
 
 const blogRouter =express.Router();
 
 blogRouter.post("/add",upload.single('image'),auth,addBlog)
-
-
+blogRouter.get('/all',getAllBlogs);
+blogRouter.get('/:blogId',getBlogById);
+blogRouter.post('/delete',auth,deleteBlogById);
+blogRouter.post('/toggle-publish',auth,togglePublished);
+blogRouter.post('/add-comment',addComment);
+blogRouter.post('/comments',getBlogComments);
 export default blogRouter;
